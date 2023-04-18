a796cef5273b04500eac35df8d16c4c06535cba5
HamdiTliba <hamditliba.go@gmail.com>
2023-04-18 16:10:51 +0100

    add routes

--- Server/EcommerceAPI/index.js
+++ Server/EcommerceAPI/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.get("/api/test", () => {
 });
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
